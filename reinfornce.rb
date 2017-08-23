8d7a1dd355de11afebaff8cfbb3632685529d623
Andy Tran <andy8tran@gmail.com>
2017-08-23 14:30:45 -0400

    done

---
+++ reinfornce.rb
@@ -0,0 +1,21 @@
+hash = { data: { rooms:
+    [ { id: 1, room_number: "201", capacity: 50}, { id: 2, room_number: "301", capacity: 200 }, { id: 3, room_number: "1B", capacity: 100}
+    ],
+    events: [
+      { id: 1, room_id: 2, start_time: 18, end_time: 20, attendees: 75 },
+      { id: 2, room_id: 1, start_time: 10, end_time: 18, attendees: 25 },
+      { id: 3, room_id: 2, start_time: 10, end_time: 18, attendees: 20 },
+      { id: 4, room_id: 3, start_time: 18, end_time: 21, attendees: 56 },
+    ]
+  }
+}
+
+puts hash[:data][:rooms][0][:capacity]
+
+hash[:data][:events].each do |event|
+
+  if event[:room_id] == hash[:data][:rooms][0][:id]
+    puts event[:attendees] <= hash[:data][:rooms][0][:capacity] ? "ok" : nil
+  end
+
+end
